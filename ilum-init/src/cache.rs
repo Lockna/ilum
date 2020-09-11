e2e39cba7cd3acd903320fecb7b8151e0184e4b1
Raphael O <raphael.ob@protonmail.com>
2020-09-11 13:24:29 +0200

    Create cache.rs, fault.rs, fpu.rs and nvic.rs. Add init function to each file. Init in _start

---
+++ ilum-init/src/cache.rs
@@ -0,0 +1,5 @@
+pub unsafe fn init() {
+
+
+
+}
--- ilum-init/src/lib.rs
+++ ilum-init/src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#![no_std]
+
+mod cache;
+mod fault;
+mod fpu;
+mod nvic;
+
+#[no_mangle]
+pub unsafe extern "C" fn _start() -> ! {
+	cache::init();
+	fpu::init();
+	nvic::init();
+
+	extern "Rust" {
+        fn main() -> !;
     }
+
+    #[inline(never)]
+    fn trampoline() -> ! {
+        unsafe { main() };
+    }
+
+    trampoline();
+
 }
