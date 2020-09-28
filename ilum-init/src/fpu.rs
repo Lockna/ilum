c2dcd06fed27e6ef307d7cb17880560abf32f5d4
Raphael O <raphael.ob@protonmail.com>
2020-09-28 08:56:34 +0200

    Enable the FPU

--- ilum-init/src/fpu.rs
+++ ilum-init/src/fpu.rs
@@ -1,5 +1,12 @@
+use core::ptr;
+
+#[inline(always)]
 pub unsafe fn init() {
 
+    const SCB_CPACR: *mut u32 = 0xE000_ED88 as *mut u32;
+    const SCB_CPACR_FPU_ENABLE: u32 = 0x00F00000;
+
+    ptr::write_volatile(SCB_CPACR, SCB_CPACR_FPU_ENABLE);
 
 
 }
